e01253e0f8288aa05de584357c31e70bff6c0fae
Beyazıt UYSAL <66223190+masanbasa3k@users.noreply.github.com>
2024-02-10 20:01:46 +0300

    webui fix and edit

--- views.py
+++ views.py
@@ -1,24 +1,21 @@
 from flask import Blueprint, render_template, request
-# import sys
-# sys.path.append("Paragesion")
 
 from take_data import concatenate_paragraphs_from_urls
 
 views = Blueprint(__name__, "views")
 input_data = None
+status = ''
 
-@views.route('/')
+@views.route('/', methods=['POST','GET'])
 def home():
-    return render_template('index.html')
-
-@views.route('/process_input', methods=['POST'])
-def process_input():
     input = request.form['input']
-    # Do something with the input data here
     input_paragraph = concatenate_paragraphs_from_urls(input)
-    return f'Input received: {input_paragraph}'
+    if input_paragraph != "":
+        return render_template('index.html', status='basariii', input_paragraph=input_paragraph)
+    else:
+        return render_template('index.html', status='data not taken', input_paragraph=input_paragraph)
 
-@views.route('/process_question', methods=['POST'])
+@views.route('/', methods=['POST'])
 def process_question():
     question = request.form['questionInput']
     # Burada alınan soruyu kullanarak istediğiniz işlemi gerçekleştirebilirsiniz.
